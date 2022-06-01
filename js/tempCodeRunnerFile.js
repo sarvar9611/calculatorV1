0ecce1014f44a34cbdf509784d58da718c7c175f
Sarvarbek Azimov <sarvar9611@yandex.com>
2022-06-01 23:55:07 +0500

    first commit

---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let letter = "SARVAR";
+    while (letter.length < 1){
+        letter = prompt("Xarfni qayta kiriting! ", "").trim();
+    }
+    letter = letter.toLowerCase();
+    console.log(letter);
